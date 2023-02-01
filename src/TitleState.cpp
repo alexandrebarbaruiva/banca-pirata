314e48763929fa56a22b2e9832e95c0a5bdb900e
Alexandre Augusto <alexandrebarbaruiva@gmail.com>
2023-02-01 10:27:39 -0300

    Change title background to official image

--- src/TitleState.cpp
+++ src/TitleState.cpp
@@ -14,8 +14,7 @@ TitleState::TitleState() : State(), backgroundMusic("assets/audio/abertura.ogg")
 {
     // Background
     GameObject *bg = new GameObject();
-    Sprite *spriteBackground = new Sprite(*bg, "assets/img/store.png", 1, 1.0f);
-    spriteBackground->SetScale(1.5, 1.5); 
+    Sprite *spriteBackground = new Sprite(*bg, "assets/img/placeholders/Grade_Anim_Start.png", 1, 1.0f);
     bg->AddComponent(spriteBackground);
     bg->box.x = 0;
     bg->box.y = 0;
